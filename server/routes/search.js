5338ca04989ef00e2289e8d52395311dbc04c2b4
inseob jeon <pravda@pravdalaptop.local>
2020-09-27 21:21:03 +0900

    modified searchDate making session with departure date and arrival date

--- server/routes/search.js
+++ server/routes/search.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 const searchControllers = require('../controllers/search/index')
 
-router.get('/:dateparams', searchControllers.searchDate.get)
+router.get('/', searchControllers.searchDate.get)
 // router.get('/article/:id',searchControllers.searchFlight)
 
 module.exports = router;
