ff2137081955c42c88209ce81cc4dbde08f2cd92
Shawn Han <tosky0514@gmail.com>
2020-10-03 21:01:52 +0900

    likes
    likes router, controllers implemented

--- server/controllers/articles/write.js
+++ server/controllers/articles/write.js
@@ -2,8 +2,9 @@ const { articles } = require('../../models')
 
 module.exports = {
   write: (req, res) => {
+    let { title, content } = req.body
+    let author = req.session.usernme;
     
-	  let { author , title, content } = req.body
     articles.create({ author : author, title : title, contents : content}).then(result => {
       res.status(201).send(JSON.stringify({status : true}))
     })
